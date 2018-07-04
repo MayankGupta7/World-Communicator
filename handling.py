0ebed22eb669e58bbdca05cb682bc692322934cd
MayankGupta7 <mg4897@gmail.com>
2018-07-04 10:52:07 +0530

    Basic Error Handling

---
+++ handling.py
@@ -0,0 +1,17 @@
+try:
+    print("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    # i = 0/0
+    f = open("abc.txt")
+    # zerodivision = 0/0
+
+except FileNotFoundError as e:
+    print(e.filename, "is not present")
+    print(dir(e))
+    
+# except (ZeroDivisionError, FileNotFoundError):                                        # except Exception as e:
+#     print("You have divided by zero")
+#     # print("ZeroDivisionError")
+# except RuntimeError:
+#     print("This is a Run Time Error")
+# except FileNotFoundError:
+#     print("File is not found")
